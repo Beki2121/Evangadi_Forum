ca0750f31b359bbb527c84a8ed38c762f2125461
selamawitberhanu <selamawitberhanu@gmail.com>
2025-06-09 14:31:02 +0200

    db-config

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -9,5 +9,14 @@ const dbConnection = mysql2.createPool({
   connectionLimit: process.env.CONNECTION_LIMIT,
   host: process.env.DB_HOST,
 });
-
+// Test the database connection
+dbConnection.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection error:", err);
+    process.exit(1);
+  } else {
+    console.log("Database connected successfully.");
+    connection.release(); // Release the connection back to the pool
+  }
+});
 module.exports = dbConnection.promise();
